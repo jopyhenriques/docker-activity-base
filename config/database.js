28c2047961267218105d1c732b412e6569276c9d
jopyhenriques <jopy.henriques@gmail.com>
2017-09-19 17:35:24 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 
-	url : 'mongodb://mongodemo:27017/todo'
+	uri: 'mongodb://mongodemojopy:rD95TaYvPbaQHXlzO991f0Llgmn5Dh0aqftsFxbwBTT36pwdWSRb6WBLqQu15A8hGhJ2CYHMrLFF3nlz0QxTVQ==@mongodemojopy.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false'
 }
