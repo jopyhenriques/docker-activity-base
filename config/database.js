5b4a967ed6990751b845c4c40734cd306ddeb93d
jopyhenriques <jopy.henriques@gmail.com>
2017-09-19 17:36:34 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 
-	uri: 'mongodb://mongodemojopy:rD95TaYvPbaQHXlzO991f0Llgmn5Dh0aqftsFxbwBTT36pwdWSRb6WBLqQu15A8hGhJ2CYHMrLFF3nlz0QxTVQ==@mongodemojopy.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false'
+	uri: 'mongodb://mongodemojopy:rD95TaYvPbaQHXlzO991f0Llgmn5Dh0aqftsFxbwBTT36pwdWSRb6WBLqQu15A8hGhJ2CYHMrLFF3nlz0QxTVQ==@mongodemojopy.documents.azure.com:10250/todo'
 }
